8d8e49ab9e4fa36d098db32e46b97a5810901e05
Franco Parente <fparente14@frba.utn.edu.ar>
2021-08-26 20:08:22 -0300

    base de conocimientos

---
+++ Lógico/Parciales/Sueños/sueños.pl
@@ -0,0 +1,36 @@
+%--------
+% Sueños
+%--------
+
+% Punto 1
+
+%persona(Persona).
+persona(gabriel).
+persona(juan).
+persona(macarena).
+persona(diego).
+
+%creencia(Creencia).
+creencia(campanita).
+creencia(magoOz).
+creencia(cavenaghi).
+creencia(conejoPascua).
+creencia(reyesMagos).
+creencia(magoCapria).
+
+%cree(Persona, Creencia).
+cree(gabriel,campanita).
+cree(gabriel,magoOz).
+cree(gabriel,cavenaghi).
+cree(juan,conejoPascua).
+cree(macarena,reyesMagos).
+cree(macarena,magoCapria).
+cree(macarena,campanita).
+
+%suenio(Persona,Functor).
+%tipoSuenio(TipoSuenio,Parametro).
+suenio(gabriel,ganarLoteria([5,9])).
+suenio(gabriel,serFutbolista(arsenal)).
+suenio(juan,serCantante(100000)).
+suenio(macarena,serCantante(10000)).
+
