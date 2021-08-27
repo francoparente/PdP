b624f8dca7255d5063daa7600d40b5c8269bae71
Franco Parente <fparente14@frba.utn.edu.ar>
2021-08-26 21:30:48 -0300

    punto 2 terminado (gracias luchin)

--- Lógico/Parciales/Sueños/sueños.pl
+++ Lógico/Parciales/Sueños/sueños.pl
@@ -28,8 +28,9 @@ cree(macarena,magoCapria).
 cree(macarena,campanita).
 
 %suenio(Persona,Functor).
+
 %ganarLoteria(SerieNumeros).
-%serFutbolista(NombreEquipo).
+%serFutbolista(Equipo).
 %serCantante(CantDiscosVendidos).
 
 suenio(gabriel,ganarLoteria([5,9])).
@@ -37,6 +38,20 @@ suenio(gabriel,serFutbolista(arsenal)).
 suenio(juan,serCantante(100000)).
 suenio(macarena,serCantante(10000)).
 
+%equipoChico(Equipo).
+equipoChico(arsenal).
+equipoChico(aldosivi).
+
+tipoSuenio(TipoSuenio) :-
+    suenio(Persona,_).
+
 %dificultad(TipoSuenio, Dificultad).
-dificultad(serCantante(CantDiscos),6) :- CantDiscos >500000.
-dificultad(serCantante(CantDiscos),4) :- CantDiscos =<500000.
+dificultad(Persona,6) :- suenio(Persona,serCantante(CantDiscosVendidos)), CantDiscosVendidos>500000.
+dificultad(Persona,4) :- suenio(Persona,serCantante(CantDiscosVendidos)), CantDiscosVendidos=<500000.
+
+dificultad(Persona,Dificultad) :- suenio(Persona,ganarLoteria(SerieNumeros)), length(SerieNumeros,CantidadNumeros), Dificultad is CantidadNumeros*10.
+
+dificultad(Persona,3) :- suenio(Persona,serFutbolista(Equipo)), equipoChico(Equipo).
+dificultad(Persona,4) :- suenio(Persona,serFutbolista(Equipo)), not(equipoChico(Equipo)).
+
+esAmbiciosa(Persona) :- persona(Persona), findall(Dificultad,dificultad(Persona,Dificultad),DificultadTotal), sum_list(DificultadTotal,Suma), Suma >20.
