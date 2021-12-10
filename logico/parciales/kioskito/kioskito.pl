65aa1ec0a4b9bd81deff2ac95a050e3c2e3ede61
Franco Parente <fparente14@frba.utn.edu.ar>
2021-12-10 05:22:58 -0300

    feat: punto 1

--- logico/parciales/kioskito/kioskito.pl
+++ logico/parciales/kioskito/kioskito.pl
@@ -8,3 +8,12 @@ atiende(juanFdS,viernes,(12,20)).
 atiende(leoC,lunes,(14,18)).
 atiende(leoC,miercoles,(14,18)).
 atiende(martu,miercoles,(23,24)).
+
+atiende(vale,Dia,Hora) :-
+    atiende(dodain,Dia,Hora).
+atiende(vale,Dia,Hora) :-
+    atiende(juanC,Dia,Hora).
+
+% nadie hace el mismo horario que leoC.
+% maiu está pensando si hace el horario de 0 a 8 los martes y miércoles.
+% NO VA EN LA BASE DE CONOCIMIENTO POR EL PRINCIPIO DE UNIVERSO CERRADO.
