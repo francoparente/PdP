c11394b562806f3582f186749993f146e1a7362f
Franco Parente <fparente14@frba.utn.edu.ar>
2021-12-10 23:08:52 -0300

    feat: punto2 + tests

--- logico/parciales/kioskito/kioskito.pl
+++ logico/parciales/kioskito/kioskito.pl
@@ -1,22 +1,28 @@
 % --------------- Base de conocimiento
 
-atiende(dodain,lunes,(9,15)).
-atiende(dodain,miercoles,(9,15)).
-atiende(lucas,martes,(10,20)).
-atiende(juanC,sabados,(18,22)).
-atiende(juanC,domingos,(18,22)).
-atiende(juanFdS,jueves,(10,20)).
-atiende(juanFdS,viernes,(12,20)).
-atiende(leoC,lunes,(14,18)).
-atiende(leoC,miercoles,(14,18)).
-atiende(martu,miercoles,(23,24)).
+atiende(dodain,lunes,9,15).
+atiende(dodain,miercoles,9,15).
+atiende(dodain,viernes,9,15).
+
+atiende(lucas,martes,10,20).
+
+atiende(juanC,sabado,18,22).
+atiende(juanC,domingo,18,22).
+
+atiende(juanFdS,jueves,10,20).
+atiende(juanFdS,viernes,12,20).
+
+atiende(leoC,lunes,14,18).
+atiende(leoC,miercoles,14,18).
+
+atiende(martu,miercoles,23,24).
 
 % --------------- Punto 1
 
-atiende(vale,Dia,Hora) :-
-    atiende(dodain,Dia,Hora).
-atiende(vale,Dia,Hora) :-
-    atiende(juanC,Dia,Hora).
+atiende(vale,Dia,Desde,Hasta) :-
+    atiende(dodain,Dia,Desde,Hasta).
+atiende(vale,Dia,Desde,Hasta) :-
+    atiende(juanC,Dia,Desde,Hasta).
 
 % nadie hace el mismo horario que leoC.
 % maiu está pensando si hace el horario de 0 a 8 los martes y miércoles.
@@ -24,8 +30,9 @@ atiende(vale,Dia,Hora) :-
 
 % --------------- Punto 2
 
-quienAtiende(Dia,Hora,Persona) :-
-    atiende(Persona,Dia,Hora).
+quienAtiende(Persona,Dia,Hora) :-
+    atiende(Persona,Dia,HoraInicio,HoraFin),
+    between(HoraInicio,HoraFin,Hora).
 
 % --------------- Punto 3
 
@@ -37,3 +44,24 @@ quienAtiende(Dia,Hora,Persona) :-
 
 % --------------- Punto 5
 
+
+
+% --------------- Tests
+
+:- begin_tests(kioskito).
+
+test(atienden_los_viernes, set(Persona = [vale,dodain,juanFdS])):-
+    atiende(Persona,viernes,_,_).
+
+test(personas_que_atienden_un_dia_y_hora_determinada, set(Persona = [vale, dodain, leoC])) :-
+    quienAtiende(Persona,lunes,14).
+test(personas_que_atienden_un_dia_y_hora_determinada, set(Persona = [vale, juanC])) :-
+    quienAtiende(Persona,sabado,18).
+
+test(atiende_a_una_hora_determinada, true) :-
+    quienAtiende(juanFdS,jueves,11).
+
+test(dias_que_atiende_una_persona_en_un_horario_determinado, set(Dia = [lunes,miercoles,viernes])) :-
+    quienAtiende(vale,Dia,10).
+
+:-end_tests(kioskito).
